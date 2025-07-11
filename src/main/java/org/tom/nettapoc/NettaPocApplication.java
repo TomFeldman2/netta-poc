acffe1a207725f45dfe8340b3d8da7f32cadbba2
TomFeldman2 <tom.p@cmpus.technion.ac.il>
2025-07-11 12:55:08 +0300

    init

---
+++ src/main/java/org/tom/nettapoc/NettaPocApplication.java
@@ -0,0 +1,13 @@
+package org.tom.nettapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettaPocApplication.class, args);
+    }
+
+}
